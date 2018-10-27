c911b1cc132dcbbab7e1437e20e8be028e5b6066
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2018-10-28 00:23:30 +0100

    Turtle simulation

--- teleop/script/main.py
+++ teleop/script/main.py
@@ -7,18 +7,21 @@ from geometry_msgs.msg import Twist
 class Adapter:
     def __init__(self):
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.cur_velocity = Twist()
 
     def callback(self, joy_msg):
-        velocity = Twist()
-        velocity.linear.x = 0.1 * joy_msg.axes[1]
-        velocity.angular.z = 0.1 * joy_msg.axes[0]
-        self.cmd_vel_pub.publish(velocity)
+        self.cur_velocity.linear.x = rospy.get_param("~coef", 0.1) * joy_msg.axes[1]
+        self.cur_velocity.angular.z = rospy.get_param("~coef", 0.1) * joy_msg.axes[3]
+        self.cmd_vel_pub.publish(self.cur_velocity)
 
     def listener(self):
         rospy.init_node('joy_to_p2os_adapter', anonymous=True)
         rospy.Subscriber("joy", Joy, self.callback)
         rospy.loginfo("Listener ready")
-        rospy.spin()
+        rate = rospy.Rate(5)  # 5hz
+        while not rospy.is_shutdown():
+            self.cmd_vel_pub.publish(self.cur_velocity)
+            rate.sleep()
 
 
 if __name__ == '__main__':
