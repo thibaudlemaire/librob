2599c8693a93d2730f743254cdf307e12b4d764e
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2018-11-09 10:18:19 +0000

    Pre-merge readying

--- db_adapter/scripts/db_client.py
+++ db_adapter/scripts/db_client.py
@@ -4,22 +4,26 @@ import sys
 import rospy
 from librarian_msgs.srv import *
 
+
 def db_comms_client(request):
 	rospy.wait_for_service('perform_database_request')
 	try:
 		perform_database_request = rospy.ServiceProxy('perform_database_request', db_request)
 		resp1 = perform_database_request(request)
 		return resp1.books
-	except rospy.ServiceException, e:
-		print "Service call failed: %s" %e
+	except rospy.ServiceException as e:
+		print("Service call failed: %s" % e)
+
+
 def usage():
-	return "%s name"%sys.argv[0]
+	return "%s name" % sys.argv[0]
+
 
 if __name__ == "__main__":
 	if len(sys.argv) == 2:
 		request = str(sys.argv[1])
 	else:
-		print usage
+		print(usage)
 		sys.exit(1)
-	print "Requesting code for: %s"%(request)
-	print "code for %s is %s"%(request,db_comms_client(request))
+	print("Requesting code for: %s" % request)
+	print("code for %s is %s" % (request, db_comms_client(request)))
--- db_adapter/scripts/db_server.py
+++ db_adapter/scripts/db_server.py
@@ -2,17 +2,34 @@
 
 from librarian_msgs.srv import *
 import rospy
+import json
+
 
 def handle_db_comms(req):
-	print "Returning code"	
-	books =  "{ books: [{   title: Machine learning,author: Mitchell, Tom M. (Tom 		Michael),code: 006.31 MIT,floor: 1,available: true },{   title: Red seas under red 		skies,author: Lynch, Scott,code: 800 LYN floor: 5,available: true }]}"
-	return db_requestResponse(books)
+	print("Returning code")
+	books = {'books': [
+		{
+			'title': 'Machine learning',
+			'author': 'Mitchell, Tom M. (Tom Michael)',
+			'code': '006.31 MIT',
+			'floor': 1,
+			'available': True
+		},{
+			'title': 'Red seas under red skies',
+			'author': 'Lynch, Scott', 'code': '800 LYN',
+			'floor': 5,
+			'available': True
+		}
+	]}
+	return db_requestResponse(json.dumps(books))
+
 
 def db_comms_server():
 	rospy.init_node('db_comms_server')
 	s = rospy.Service('perform_database_request', db_request, handle_db_comms)
-	print "Ready to convert."
+	print("Ready to convert.")
 	rospy.spin()
+
 
 if __name__ == "__main__":
 	db_comms_server()
