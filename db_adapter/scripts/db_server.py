9c88a4243f54a734f5fe351cb30d6843dabbcd17
Eliot Makabu <em3215@ic.ac.uk>
2018-11-21 17:57:23 +0000

    latest fixes

--- db_adapter/scripts/db_server.py
+++ db_adapter/scripts/db_server.py
@@ -9,9 +9,11 @@ import json
 def handle_db_comms(req):
 	print "Returning code"
 	
-	book_list = {'books':{'title':'', 'code':'', 'author':'', 'availability':'', 'floor':''}}
+	book_list = {'books': []}
 	
-	payload = {'vid' : 'ICL_VU1', 'tab' : 'all', 'scope' : 'LRSCOP_44IMP', 'q' : 'title,contains,%s'%(req.request), 'lang': 'eng', 'offset' : '0', 'limit' : '1', 'sort' : 'rank', 'pcAvailability' : 'true', 'conVoc' : 'false', 'inst' : '44IMP', 'mfcet' : 'rtype,include,books,1', 'mfacet' : 'tlevel,include,available,2', 'mfacet' : 'library,include,44IMP_CENTRAL_LIB,3', 'lang' : 'en_US', 'mode' : 'Basic', 'apikey' : 'l7xx84a80e38fc0f482c9fdee23e3ede69f1'}
+	payload = {'vid' : 'ICL_VU1', 'tab' : 'all', 'scope' : 'LRSCOP_44IMP', 'q' : 'title,contains,%s'%(req.request), 'lang': 'eng', 'offset' : '0', 'limit' : '5', 'sort' : 'rank', 'inst' : '44IMP', 'mfacet' : 'tlevel,include,available,2', 'mfacet' : 'library,include,44IMP_CENTRAL_LIB,3', 'mfacet' : 'rtype,include,books,1', 'lang' : 'en_US', 'mode' : 'Basic', 'apikey' : 'l7xx84a80e38fc0f482c9fdee23e3ede69f1', 'pcAvailability' : 'true',  'conVoc' : 'true'}
+
+	#,
 	
 
 	resp = requests.get('https://api-eu.hosted.exlibrisgroup.com/primo/v1/search', params = payload)
@@ -19,21 +21,29 @@ def handle_db_comms(req):
 	if resp.status_code != 200:
 		raise ApiError('GET /primo/v1/search/ {}' .format(resp.status_code))	
 	
+	
+	
+	data = resp.json()
+	#print(len(data["docs"]))
 
-	books =  "{ books: [{   title: Machine learning,author: Mitchell, Tom M. (Tom 		Michael),code: 006.31 MIT,floor: 1,available: true },{   title: Red seas under red 		skies,author: Lynch, Scott,code: 800 LYN floor: 5,available: true }]}"	
+
+	for i in range(len(data["docs"])):
+		book = dict()
+		if (data["docs"][i]["pnx"]["display"]["type"][0] == "book" and data["docs"][i]["delivery"]["bestlocation"]["subLocation"].startswith("L") ):
 	
-	data = resp.json()	
-	
-	book_list['books']['code'] = data["docs"][0]["delivery"]["bestlocation"]["callNumber"]
-	book_list['books']['title'] = data["docs"][0]["pnx"]["display"]["title"]
-	book_list['books']['author'] = data["docs"][0]["pnx"]["display"]["creator"]
-	book_list['books']['availability'] = data["docs"][0]["delivery"]["bestlocation"]["availabilityStatus"]
-	book_list['books']['floor'] = data["docs"][0]["delivery"]["bestlocation"]["subLocation"]
+			book['code'] = data["docs"][i]["delivery"]["bestlocation"]["callNumber"][1:-2]
+			book['title'] = data["docs"][i]["pnx"]["display"]["title"][0]
+			book['author'] = data["docs"][i]["pnx"]["sort"]["author"][0]
+			book['available'] = (data["docs"][i]["delivery"]["bestlocation"]["availabilityStatus"] == "available")
+			book['floor'] = int(data["docs"][i]["delivery"]["bestlocation"]["subLocation"].split(" ")[1])
+			book_list["books"].append(book)
+		#print(i)
 	
 	json_list = json.dumps(book_list, indent = 4, sort_keys = True)
 	
 	print json_list
-	return db_requestResponse(books)
+	print(resp.url)
+	return db_requestResponse(json.dumps(book_list))
 
 def db_comms_server():
 	rospy.init_node('db_comms_server')
