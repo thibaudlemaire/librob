75a54514843ffef09cc81793b281cda9fca8018a
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2018-11-23 17:38:05 +0000

    Adding locator and database

--- locator/scripts/locator.py
+++ locator/scripts/locator.py
@@ -1,26 +1,65 @@
-#!/usr/bin/env python
-
-from librarian_msgs.srv import *
-from geometry_msgs.msg import Pose, Point, Quaternion
-import rospy
+import yaml
 
 
-def handle_requests(req):
-	response = book_locatorResponse()
-	response.floor = 1
-	response.pose = Pose()
-	response.pose.position = Point(100, 104, 0)
-	response.pose.orientation = Quaternion(0,0,0,1)
-	return response
+class Locator:
+    """Locator to find position of a book in a library
+    Initially implemented on the Imperial College Library,
+    is based on a yaml file containing the first book of a shelve
+    and the position of the shelve.
+    """
+    MAX_CODE_LENGTH = 9
 
+    def __init__(self,shelveCatalogue):
+        self.dictBookList = yaml.load(open(shelveCatalogue))
+        self.shelves = list()
+        for key in self.dictBookList:
+            self.presentShelve = list()
+            self.numericalCode = self.numericalCodeGenerator(key)
+            self.presentShelve.append(self.numericalCode)
+            self.presentShelve.extend(self.dictBookList[key])
+            self.shelves.append(self.presentShelve)
+            #print(self.shelves)
+            #print (dictBookList[key])
+        self.shelves.sort()
+        #print(self.shelves)
 
-def locator_node():
-	rospy.init_node('locator_node')
-	s = rospy.Service('locate_book', book_locator, handle_requests)
-	print("Locator node ready")
-	rospy.spin()
+    def numericalCodeGenerator(self, key):
+        """Takes code relating to a book and returns numerical format for ordering
+        A code in the form of an integer is necessary to define what books come first
+        and where they are located.
+        Args:
+            the code as returned from the lib server
+        Returns:
+             a numerical code to numerically order the books
+             format of the numerical code is:
+             9 digits containing as many numerical digits present on the original code
+             and reported in the same format plus as many zeros as necessary to fill the
+             code with 9 digits
+             6 digits representing the conversion of the alphabetical triplete
+        """
+        keyClean = key.replace(".", "")  # delete dots from book code
+        numbers, characters = keyClean.split(' ')
+        compZeros = '0' * (self.MAX_CODE_LENGTH - len(numbers))
+        unicodeCharacters = [str(ord(c)) for c in characters.upper()]
+        numericalCode = int(numbers + compZeros + ''.join(unicodeCharacters))
+        return numericalCode
+
+    def returnLocation(self, bookCode):
+        """Takes a book code and returns the position of the shelve
+        that contains it
+        """
+        numericalBookCode = self.numericalCodeGenerator(bookCode)
+        #print(numericalBookCode)
+        location, orientation = ([0,0,0], [0,0,0,1])
+        for indexShelve in range(len(self.shelves)):
+            if numericalBookCode < self.shelves[indexShelve][0]:
+                location = self.shelves[indexShelve-1][1]
+                orientation = self.shelves[indexShelve-1][2]
+                break
+        return location, orientation
 
 
 if __name__ == "__main__":
-	locator_node()
-
+    example = Locator("lib_ICL_5_floor.yaml")
+    location, orientation = example.returnLocation("722.23 EDF")
+    print(location)
