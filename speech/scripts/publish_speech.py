a47fb4891bff90a3c5a9dcd298d19aa28ac2e73d
Cao An Le <cal115@ic.ac.uk>
2018-11-23 13:23:35 +0000

    added launch file for speech module

--- speech/scripts/publish_speech.py
+++ speech/scripts/publish_speech.py
@@ -23,7 +23,21 @@ class Speech:
             return True, text
         except Exception as e:
             return False, e
-        
+
+    def publish(self, topic, msg_type, msg_payload):
+        if topic == 'ui_command':
+            msg = UI()
+            msg.type = msg_type
+            msg.paylod = msg_payload
+            self.pub_command.publish(msg)
+        else topic == 'ui_feedback':
+            msg = UI_feedback()
+            msg.type = msg_type
+            msg.payload = msg_payload
+            self.pub_feedback.publish(msg)
+        else:
+            print('Error: attempt to publish on unknown topic.')
+
 
     # callback when node reecives a message on the ui_command topic
     def sub_command_callback(self, UI_msg): # UI_msg: message given by the data that is passed
@@ -34,9 +48,7 @@ class Speech:
             with sr.Microphone() as source:
             
                 # Tell the UI_feedback that his speech is being listened with ui_feedback message of type LISTENING
-                msg = UI_feedback()
-                msg.type = UI_feedback.LISTENING
-                self.pub_feedback.publish(msg)
+                publish('ui_command', UI.LISTENING, '')
 
                 self.r.adjust_for_ambient_noise(source)
                 self.r.dynamic_energy_threshold = True
@@ -44,25 +56,17 @@ class Speech:
                 recognised, txt = recogTest(source)
 
                 if recognised:
-                    msg = UI()
-                    msg.type = UI.SEARCH_REQUEST
-                    msg.payload = json.dumps({'request': txt})
-                    self.pub_command.publish(msg)
+                    publish('ui_command', UI.SEARCH_REQUEST, json.dumps({'request': txt}))
                     print('you said:', txt)
                 else:
-                    msg = UI()
-                    msg.type = UI.NOT_UNDERSTOOD
-                    self.pub_feedback.publish(msg)
+                    publish('ui_feedback', UI.NOT_UNDERSTOOD, '')
                     print('error:', txt)
 
-
-        
             
     def startNode(self):
         # subscriber object of the ui_command topic
         self.sub_command = rospy.Subscriber('ui_command', UI, self.sub_command_callback) # (topic, messsage, callback)
         rospy.spin() # keeps python from exiting until this node is stopped
-
 
 
 
