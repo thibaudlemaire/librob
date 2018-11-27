826fee07db5935caec610f89eface1ad1093bcf6
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2018-11-27 11:22:05 +0000

    Messages update

--- behaviour/scripts/messages.py
+++ behaviour/scripts/messages.py
@@ -1,6 +1,8 @@
 class Messages:
-    SEARCHING = "Hmm... let me search for this book..."
-    NOT_FOUND = "Sorry, I cannot find corresponding book"
+    SEARCHING = "Hmm... let me search for"
+    NOT_FOUND = "Sorry, I did not find any books for"
+    FOUND = "Here is what I found for"
     DB_ERROR = "Sorry, I cannot request the database"
     LOCATOR_ERROR = "Sorry, I cannot find the location of this book"
     NOT_UNDERSTOOD = "Sorry, I did not understand what you said"
+    FOLLOW_ME = "Let's go ! Please follow me"
