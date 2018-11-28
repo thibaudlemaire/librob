cbf290297b61aad7a42738f99f4667e55b9d5aa6
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2018-11-28 13:30:56 +0000

    Implementing state machine skeleton

--- behaviour/scripts/messages.py
+++ behaviour/scripts/messages.py
@@ -6,3 +6,4 @@ class Messages:
     LOCATOR_ERROR = "Sorry, I cannot find the location of this book"
     NOT_UNDERSTOOD = "Sorry, I did not understand what you said"
     FOLLOW_ME = "Let's go ! Please follow me"
+    BUSY = "Sorry, I'm still moving and cannot process your request"
---
+++ behaviour/scripts/state.py
@@ -0,0 +1,6 @@
+class State(object):
+    def __init__(self, behaviour_node):
+        self.node = behaviour_node
+
+    def on_event(self, event):
+        pass
