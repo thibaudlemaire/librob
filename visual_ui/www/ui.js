419c50f0d0e01c8569933a36ee0dd226aa718c55
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2018-12-07 17:27:35 +0000

    Debug thumbnail

--- visual_ui/www/ui.js
+++ visual_ui/www/ui.js
@@ -146,8 +146,8 @@ function createTable(books) {
         var row = tbody.insertRow(-1);
 
         var cell = row.insertCell();
-        cell.setAttribute('class', 'align-middle thumbnail');
-        cell.innterHTML = "<img class='img-rounded img-responsive' src='" + book['thumbnail'] + "' >";
+        cell.setAttribute('class', 'align-middle thumbnail p-1');
+        cell.innerHTML = '<img class="img-rounded img-responsive" src="' + book['thumbnail'] + '" />';
 
         var cell = row.insertCell();
         cell.setAttribute('class', 'align-middle title');
